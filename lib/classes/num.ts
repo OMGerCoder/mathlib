39131346b6e08caedf9b9be2009e4d8239707881
Hugh Stuckless <hugh.stuckless@hotmail.com>
2020-05-20 13:27:44 -0400

    idk

--- lib/classes/num.ts
+++ lib/classes/num.ts
@@ -1,18 +1,34 @@
+interface INum {
+    add(x: Number): Num;
+    sub(x: Number): Num;
+}
+
 /** Class representing a number. */
-export class Num {
-    value: any
+export class Num implements INum {
+    value: number;
     /** Create a number with a certain value. */
-    constructor(value?: Number) {
+    constructor(value: number = 0) {
         this.value = value;
+    }
+
+    /**
+     * Subtracts a number from the current value, and returns the new object.
+     * @param {number} x The number to subtract
+     * @returns {Num} The new object.
+     */
+    sub(x: number): Num {
+        this.value += x;
+        return this;
     }
     
     /**
-     * Adds a number to the current value, and returns the new value.
+     * Adds a number to the current value, and returns the new object.
      * @param {number} x The number to add.
+     * @returns {Num} The new object.
      */
-    add(x: Number): Number {
-        this.value += x;
-        return this.value;
+    add(x: number): Num {
+        this.value -= x;
+        return this;
     }
 
 }
