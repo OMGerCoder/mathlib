61d7515bc91cbc1c625fc1916eded06049b5cf2b
Hugh Stuckless <hugh.stuckless@hotmail.com>
2020-05-20 09:26:19 -0400

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+const expect = require('chai').expect;
+const index = require('../dist/index');
+
+describe('functions test', () =>{
+    it('should say 4 + 2 = 6', () =>{
+        var result = index.add(4, 2);
+        expect(result).to.equal(6);
+    });
+    it('should say 4 - 2 is 2', () => {
+        
+    })
+});
