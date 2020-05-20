c02ae76207b3ee5af1cc80e248fee41e5ef16f42
Hugh Stuckless <hugh.stuckless@hotmail.com>
2020-05-20 17:43:57 -0400

    added more tests

--- test/test.js
+++ test/test.js
@@ -1,14 +1,35 @@
 'use strict';
 const expect = require('chai').expect;
-const index = require('../dist/index');
+const mathlib = require('../dist/index');
 
-describe('functions test', () =>{
+describe('generic functions test', () =>{
     it('should say 4 + 2 = 6', () =>{
-        var result = index.add(4, 2);
+        var result = mathlib.add(4, 2);
         expect(result).to.equal(6);
     });
     it('should say 4 - 2 is 2', () => {
-        var result = index.sub(4, 2);
+        var result = mathlib.sub(4, 2);
         expect(result).to.equal(2)
-    })
+    });
+});
+describe('num class test', () =>{
+    it('should create a number with a value of 6', () => {
+        var result = new mathlib.Num(6);
+        expect(result.value).to.equal(6);
+    });
+    it('should create a number with a value of 0', () => {
+        var result = new mathlib.Num();
+        expect(result.value).to.equal(0);
+    });
+    it('should create a number with a value of 6 (starting with 0)', () => {
+        var result = new mathlib.Num();
+        result.add(6);
+        expect(result.value).to.equal(6);
+    });
+    
+    it('should create a number with a value of 6 (starting with 10)', () => {
+        var result = new mathlib.Num(10);
+        result.sub(4);
+        expect(result.value).to.equal(6);
+    });
 });
