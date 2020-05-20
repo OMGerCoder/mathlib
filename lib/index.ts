f33df4dc567bd8788afe0cfe6d7d8f93f3f1534d
Hugh Stuckless <hugh.stuckless@hotmail.com>
2020-05-20 11:45:34 -0400

    split files

--- lib/index.ts
+++ lib/index.ts
@@ -1,20 +1,4 @@
 'use strict';
-/**
- * Adds 2 numbers together.
- * @param {number} a Number 1
- * @param {number} b Number 2
- * @returns {number} The numbers added.
- */
-export function add(a: number, b: number): number {
-    return a + b;
-};
 
-/**
- * Subtracts number B from number A.
- * @param {number} a Number 1
- * @param {number} b Number 2
- * @returns {number} Number B subtracted from number A.
- */
-export function sub(a: number, b: number): number {
-    return a - b;
-};
+export { add } from './operations/add';
+export { sub } from './operations/sub';
---
+++ lib/operations/add.ts
@@ -0,0 +1,10 @@
+'use strict';
+/**
+ * Adds 2 numbers together.
+ * @param {number} a Number 1
+ * @param {number} b Number 2
+ * @returns {number} The numbers added.
+ */
+export function add(a: number, b: number): number {
+    return a + b;
+};
